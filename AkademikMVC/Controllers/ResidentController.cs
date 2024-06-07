bd5af956cdb78249dfd94bb2b17a7f97d41a5735
Dawo9889 <dawid.spolecznosci@gmail.com>
2024-06-07 23:34:54 +0200

    Przeniosłem całą logike dodawania zdjęcia do service

--- AkademikMVC/Controllers/ResidentController.cs
+++ AkademikMVC/Controllers/ResidentController.cs
@@ -1,8 +1,6 @@
 ﻿using Akademik.Application.Services;
 using Akademik.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Formats.Jpeg;
@@ -31,22 +29,6 @@ namespace AkademikMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (resident.ResidentDetails.PhotoData != null && resident.ResidentDetails.PhotoData.Length > 0)
-                {
-                    using (var image = Image.Load(resident.ResidentDetails.PhotoData.OpenReadStream()))
-                    {
-                        //Zmiana rozmiaru obrazu
-                        image.Mutate(x => x.Resize(400, 300));
-
-                        // Kompresuj obraz
-                        using (var memoryStream = new MemoryStream())
-                        {
-                            image.Save(memoryStream, new JpegEncoder { Quality = 80 }); 
-
-                            resident.ResidentDetails.Photo = memoryStream.ToArray();
-                        }
-                    }
-                }
                 await _residentService.Create(resident);
                 return RedirectToAction(nameof(Create));
             }
